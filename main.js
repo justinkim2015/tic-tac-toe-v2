e7e2b46b33de27759c87abe70bf34ad8c9490a5f
justinkim2015 <justinkim2015@gmail.com>
2022-12-18 17:32:13 +0900

    Fix board class

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // Gameboard module
 const gameBoard = (() => {
-  let array = ['','','',
-               '','','',
-               '','',''];
-                  
-  return {array}
+  const values = new Array(9).fill('')
+  
+  const setCell = (index, value) => {
+    values[index] = value
+  };
+
+  const getBoard = () => {
+    return values
+  };
+
+  return {setCell, getBoard}
 })();
 
 // display module
@@ -21,38 +27,57 @@ const display = (() => {
 
 // Game logic
 const logic = (() => {
-  const change = (element, piece) => {
+  let boxList = document.querySelectorAll('.box');
+
+  const updateArray = (index, element, piece) => {
     if(element.textContent == '') {
-      element.textContent = piece
-    }
-  };
-
-  const placePiece = (piece) => {
-    boxList = document.querySelectorAll('.box');
-
-    for (let i = 0; i < boxList.length; i++) {
-      boxList[i].addEventListener('click', change.bind(this, boxList[i], piece))
+      gameBoard.setCell(index, piece) 
+      display.update(gameBoard.getBoard())
     };
   };
 
-  return { placePiece }
+  // const removeListener = (node, callback) => {
+  //   node.removeEventListener('click', callback )
+  // }
+
+  // const addListeners = (nodes, piece) => {
+  //   for (let i = 0; i < nodes.length; i++) {
+  //     let callback = () => {updateArray(nodes[i], piece)}
+  //     nodes[i].addEventListener('click', callback )
+  //     nodes[i].addEventListener('click', () => {removeListener(nodes[i], callback)} )
+  //   };
+  // };
+
+  const removeListeners = (nodes, callbacks) => {
+    nodes.forEach( (element, index) => {element.removeEventListener('click', callbacks[index])})
+    addListeners(boxList, 'O')
+  }
+
+  const addListeners = (nodes, piece) => {
+    let callbacks = []
+
+    for (let i = 0; i < nodes.length; i++) {
+      callbacks.push(() => {updateArray(i, nodes[i], piece)})
+      nodes[i].addEventListener('click', callbacks[i] )
+      // nodes[i].addEventListener('click', () => {removeListeners(nodes, callbacks)} )
+    };
+  };
+
+  const playGame = (player1, player2) => {
+    addListeners(boxList, player1.piece)
+  };
+
+  return { playGame }
 })();
-
-
 
 // Player factory
 const makePlayer = ((name, piece) => {
   return { name, piece }
 });
 
+const playerOne = makePlayer('justin', 'X')
+const playerTwo = makePlayer('maya', 'O')  
 
+display.update(gameBoard.getBoard())
 
-function taketurn() {
-  const justin = makePlayer('justin', 'X')
-  const maya = makePlayer('maya', 'O')  
-
-  display.update(gameBoard.array)
-  logic.placePiece(maya.piece)
-}
-
-taketurn()
+logic.playGame(playerOne, playerTwo)
